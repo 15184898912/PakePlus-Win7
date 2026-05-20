cf1f51412ff6516f8f45d6a91059cba21e7e954c
15184898912 <960116565@qq.com>
2026-05-20 10:25:41 +0800

    update custom js from pakeplus

---
+++ src-tauri/data/custom.js
@@ -0,0 +1,16 @@
+window.addEventListener("DOMContentLoaded",()=>{const t=document.createElement("script");t.src="https://www.googletagmanager.com/gtag/js?id=G-W5GKHM0893",t.async=!0,document.head.appendChild(t);const n=document.createElement("script");n.textContent="window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-W5GKHM0893');",document.body.appendChild(n)});{
+  "name": "我的离线工具",
+  "title": "离线版",
+  "exe": "离线工具.exe",
+  "url": "https://dfmbj8md24.coze.site",
+  "width": 1280,
+  "height": 720,
+  "hideMenu": true,
+  "noDevTools": true,
+  "noSplash": true,
+  "noWatermark": true,
+  "cacheAll": true,
+  "blockLogin": true,
+  "blockUpdate": true,
+  "singleExe": true
+}
